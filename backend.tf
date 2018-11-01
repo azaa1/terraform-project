12c4e7e996f00d0a4f8a560b8b43bd7427974650
Ahmad Ali <42782612+azaa1@users.noreply.github.com>
2018-10-31 21:21:08 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "myuniquebucket1234"
+    bucket         = "silkroadshipping.us"
     key            = "project1/mykey"
     region         = "us-east-2"
     dynamodb_table = "project1"
